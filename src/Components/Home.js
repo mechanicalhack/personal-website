1e6ae4399e3c9ead8b9ea940741b528af9d62a80
Rob Lloyd <rlloyd28@gmail.com>
2020-02-08 21:09:53 -0500

    Add Name and welcome message

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,18 @@ import '../Style/Home.css';
 
 function Home() {
     return (
-        <div className="header">
-            <h1 className="header-text">Hello</h1>
+        <div>
+            <div>
+                <h1 className="header-text">
+                    <span>Rob</span>
+                    <span className="header-last-name-text">Lloyd</span>
+                </h1>
+            </div>
+            <div className="welcome-content">
+                <h3 className="welcome-text">Welcome</h3>
+                <p className="welcome-text">I am a Full Stack Engineer. I have created this website to show some of my
+                    skills and help you get to know me a little better.</p>
+            </div>
         </div>
     );
 }
